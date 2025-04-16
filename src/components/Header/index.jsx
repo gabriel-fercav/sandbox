eac6b8d00ef6386ab1e0c2b402c359145627e5c6
gabriel-fercav <gabriel.f.cavalcante@hotmail.com>
2025-04-16 00:17:29 -0300

    Added homepage header and call to action, implemented CSS

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,21 +2,27 @@ import React from 'react'
 
 import IconButton from '_/components/IconButton'
 
+import SandboxLogoLarge from '_/assets/logo-large-white.png'
 import { FaGithub as Github } from 'react-icons/fa'
 import { FaLinkedin as Linkedin } from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <div className="flex justify-end items-center gap-3 top-0 w-full border-b-1 border-gray-800 bg-gray-900 px-15 py-5">
-      <p className="text-1xl">Sobre</p>
-      <IconButton
-        icon={<Github />}
-        onClick={() => window.open('https://github.com/gabriel-fercav')}
-      />
-      <IconButton
-        icon={<Linkedin />}
-        onClick={() => window.open('https://www.linkedin.com/in/gabriel-fercav/')}
-      />
+    <div className="flex justify-between items-center w-full border-b-1 border-stone-800 px-15 py-3">
+      <div>
+        <img className="w-40 h-auto" src={SandboxLogoLarge} alt="Logo" />
+      </div>
+      <div className="flex items-center gap-5">
+        <p className="text-1xl">Sobre</p>
+        <IconButton
+          icon={<Github />}
+          onClick={() => window.open('https://github.com/gabriel-fercav')}
+        />
+        <IconButton
+          icon={<Linkedin />}
+          onClick={() => window.open('https://www.linkedin.com/in/gabriel-fercav/')}
+        />
+      </div>
     </div>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
-import SandboxBG from '_/assets/home-image-large.jpg'
 
 import Header from '_/components/Header'
+
+import { DeepSeek } from '@lobehub/icons'
+import { OpenAI } from '@lobehub/icons'
 
 const Home = () => {
   return (
     <>
       <Header />
-      <div className="flex flex-col justify-between w-screen h-screen bg-gray-900 p-10">
-        <div>
-          <img className="rounded-2xl w-auto h-190" src={SandboxBG} />
-        </div>
-        <div className="flex flex-col justify-center w-full pl-10">
-          <h2 className="text-9xl font-bold font-sans text-white">Sandbox</h2>
-          <p className="text-2xl text-white">
-            Um espaço de experimentação com IA, onde você pode aprender idiomas, conversar com bots
-            e explorar novas formas de interagir com inteligência artificial.
+      <div className="flex flex-1 justify-between w-screen h-[70%] p-20">
+        {/* Left column */}
+        <div className="flex flex-col justify-center w-1/2 h-full border-r-1 border-stone-800 pr-40">
+          <h2 className="text-8xl font-bold font-sans text-white mb-8">O futuro chegou!</h2>
+          <p className="text-2xl text-white mb-8">
+            Um espaço para aprender, criar histórias e explorar novas formas de interagir com
+            inteligência artificial.
           </p>
-          <button className="w-80 h-15 mt-10 bg-yellow-600 rounded-2xl">Experimente agora</button>
+          <button className="w-80 h-15 mt-5 bg-[#8ddd8d] rounded-2xl text-gray-900 text-2xl font-bold hover:bg-[#8ddd8d]/80 transition duration-300">
+            Experimente agora
+          </button>
+        </div>
+
+        {/* Right column */}
+        <div className="flex flex-col justify-center w-1/2 h-full pl-20 gap-10">
+          <OpenAI.Combine size={86} type={'color'} />
+          <DeepSeek.Combine size={86} type={'color'} />
         </div>
       </div>
     </>
