508906236c037134ecfa59ea2402efe244c7fd7f
gabriel-fercav <gabriel.f.cavalcante@hotmail.com>
2025-04-16 23:03:02 -0300

    Added CSS animations and Tailwind classes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,38 @@
 import React from 'react'
 
+import SandboxLogoLarge from '_/assets/logo-large-white.png'
 import Header from '_/components/Header'
 
 import { DeepSeek } from '@lobehub/icons'
 import { OpenAI } from '@lobehub/icons'
 
+import './styles.css'
+
 const Home = () => {
   return (
     <>
       <Header />
-      <div className="flex flex-1 justify-between w-screen h-[70%] p-20">
+      <div className="wrapper">
         {/* Left column */}
-        <div className="flex flex-col justify-center w-1/2 h-full border-r-1 border-stone-800 pr-40">
-          <h2 className="text-8xl font-bold font-sans text-white mb-8">O futuro chegou!</h2>
+        <div className="column border-r-1 border-stone-800 pr-20">
+          <div>
+            <h2 className="font-bold font-sans green text-[min(5vw,96px)]">Bem-vindo ao</h2>
+            <img className="w-150 h-auto" src={SandboxLogoLarge} alt="Logo" />
+          </div>
           <p className="text-2xl text-white mb-8">
-            Um espaço para aprender, criar histórias e explorar novas formas de interagir com
-            inteligência artificial.
+            Um espaço para aprender, criar histórias e explorar novas formas de interação com
+            inteligência artificial — tudo em um só lugar. No Sandbox, você alterna entre diferentes
+            modelos de IA na mesma sessão, com liberdade total para experimentar.
           </p>
-          <button className="w-80 h-15 mt-5 bg-[#8ddd8d] rounded-2xl text-gray-900 text-2xl font-bold hover:bg-[#8ddd8d]/80 transition duration-300">
+          <button className="w-80 h-15 mt-5 bg-[#8ddd8d] rounded-2xl text-gray-900 text-2xl font-bold hover:bg-[#8ddd8d]/80 transition duration-300 animate-pulse">
             Experimente agora
           </button>
         </div>
 
         {/* Right column */}
-        <div className="flex flex-col justify-center w-1/2 h-full pl-20 gap-10">
-          <OpenAI.Combine size={86} type={'color'} />
-          <DeepSeek.Combine size={86} type={'color'} />
+        <div className="column pl-20 slide-in-right">
+          <OpenAI.Combine size={76} type={'color'} />
+          <DeepSeek.Combine size={76} type={'color'} />
         </div>
       </div>
     </>
